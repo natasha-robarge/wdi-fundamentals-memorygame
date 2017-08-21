b10028786799567adcd2bbca6fb624146dca34c0
Natasha Robarge <natasharobarge@natashas-mbp.attlocal.net>
2017-08-21 11:22:06 -0500

    Created game logic unit 8

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-
+/*
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
+*/
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+    cardsInPlay.push(cardOne);
+var cardTwo = cards[2];
+    cardsInPlay.push(cardTwo);
+
+  if(cardsInPlay.length === 2) {
+    if(cardOne === cardTwo) {
+      alert("You found a match!");
+    } else if(cardOne !== cardTwo) {
+      alert("Sorry, try again.");
+    }
+  }
